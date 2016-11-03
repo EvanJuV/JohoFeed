1daa15699c6ccfd66e0693ca0bd3c9a7a8a225f7
alumno <alumno@Mac-19.local>
2016-11-03 13:59:08 -0600

    Carga de datos y muestra pantalla de loading

--- JohoFeed/CategoriesCollectionViewController.swift
+++ JohoFeed/CategoriesCollectionViewController.swift
@@ -12,21 +12,38 @@ private let reuseIdentifier = "category"
 
 class CategoriesCollectionViewController: UICollectionViewController {
 
-    var jsonCategories : NSArray!
-    let urlCategories = URL(string: "http://10.15.219.97:3000/api/users")
+    weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    fileprivate var arrayCategories : [Category] = []
+    let urlCategories = URL(string: "http://10.15.219.97:3000/api/categories")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Loading"
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        collectionView?.backgroundView = activityIndicatorView
+        self.activityIndicatorView = activityIndicatorView
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        activityIndicatorView.startAnimating()
         
-        
-        URLSession.shared.dataTask(with: urlCategories! as URL) { data, response, error in
+        // Make asynchrounous call to API
+        let task = URLSession.shared.dataTask(with: urlCategories! as URL) { data, response, error in
+            
+            var json: Any!
+            
             guard error == nil else {
                 print(error)
                 return
@@ -36,11 +53,28 @@ class CategoriesCollectionViewController: UICollectionViewController {
                 return
             }
             
-            let json = try! JSONSerialization.jsonObject(with: data, options: [])
-            print(json)
+            // Parse JSON
+            json = try! JSONSerialization.jsonObject(with: data, options: [])
+            
+            // Cast JSON as array
+            let jsonCategories = json as! NSArray
+            
+            // Iterate over array and insert each category object
+            for jsonCategory in jsonCategories {
+                guard let category = Category(json: jsonCategory as! JSON) else {
+                    print("Error while parsing category")
+                    return
+                }
+                
+                self.arrayCategories.append(category)
+            }
+            
+            // Remove spinner and show data
+            self.activityIndicatorView.stopAnimating()
+            self.collectionView!.reloadData()
         }
-
-        // Do any additional setup after loading the view.
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,19 +96,23 @@ class CategoriesCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return arrayCategories.count == 0 ? 0 : 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return jsonCategories.count
+        return arrayCategories.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CategoryCollectionViewCell
     
         // Configure the cell
+        cell.backgroundColor = UIColor.white
+        cell.lbTitle.text = arrayCategories[indexPath.row].title
+        
+        print(arrayCategories[indexPath.row].title)
     
         return cell
     }
@@ -109,5 +147,6 @@ class CategoriesCollectionViewController: UICollectionViewController {
     
     }
     */
-
+    
 }
+
---
+++ JohoFeed/CategoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCollectionViewCell.swift
+//  JohoFeed
+//
+//  Created by alumno on 03/11/16.
+//  Copyright © 2016 Evan Juárez. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    
+}
